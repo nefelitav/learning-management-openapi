47a025f0a4cc16c40c3ad1fbb3e6b76de6993bed
nefelitav <nefeli.tavoulari123@gmail.com>
2024-02-07 22:14:57 +0100

    add service, dockerize

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,8 +9,9 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Simple Inventory API'}, pythonic_params=True)
-    app.run(port=8080)
+    return app.app
 
 
 if __name__ == '__main__':
-    main()
+    app = main()
+    app.run(port=8080, debug=True)
