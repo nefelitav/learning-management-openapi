b5dabd523c72c2faab618c989c3966705969c7e1
nefelitav <nefeli.tavoulari123@gmail.com>
2024-02-08 17:12:45 +0100

    fix dockerization issues

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,9 +9,8 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Simple Inventory API'}, pythonic_params=True)
-    return app.app
+    app.run(port=8080)
 
 
 if __name__ == '__main__':
-    app = main()
-    app.run(port=8080, debug=True)
+    main()
